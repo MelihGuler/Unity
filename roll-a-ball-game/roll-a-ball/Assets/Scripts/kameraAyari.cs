213bf6f22afe1b8c0d28038f6b5e01d149c9aa71
Melih Güler <melihguler999@gmail.com>
2019-02-09 22:23:50 +0300

    re-uploaded

---
+++ roll-a-ball-game/roll-a-ball/Assets/Scripts/kameraAyari.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kameraAyari : MonoBehaviour {
+    private Vector3 mesafe;
+    private GameObject top;
+	void Start () {
+        top = GameObject.Find("Top");
+        mesafe = transform.position - top.transform.position;
+	}
+	
+	
+	void LateUpdate () {
+        transform.position= top.transform.position+mesafe;
+	}
+}
---
+++ roll-a-ball-game/roll-a-ball/Assets/Scripts/topKontrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class topKontrol : MonoBehaviour {
+
+    private Rigidbody fizik;
+    private int sayac = 0;
+    private int toplanacakObjeSayisi = 12;
+    private Text sayacTxt,oyunBittiTxt;
+	void Start () {
+        GameObject getir = GameObject.Find("sayac");
+        sayacTxt = getir.GetComponent<Text>();
+        sayacTxt.text = "Toplanılan: 0";
+        GameObject oyunBittiGetir = GameObject.Find("oyunBitti");
+        oyunBittiTxt = oyunBittiGetir.GetComponent<Text>();
+        fizik = GetComponent<Rigidbody>();
+	}
+
+    private float yatay, dikey;
+    private Vector3 vector3;
+    void FixedUpdate () {
+         yatay = Input.GetAxisRaw("Horizontal");
+         dikey = Input.GetAxisRaw("Vertical");
+        vector3 = new Vector3(yatay, 0, dikey);
+        fizik.AddForce(vector3*10);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="engel")
+        {
+            other.gameObject.SetActive(false);
+            sayac++;
+            sayacTxt.text = "Toplanılan: " + sayac;
+        }
+        if (sayac == toplanacakObjeSayisi)
+        {
+            oyunBittiTxt.text="OYUN BİTTİ !";
+        }
+    }
+}
